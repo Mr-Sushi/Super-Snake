c0a94331063ed923b15f397caecca060a2ec5a81
unknown <shuxin02>
2020-11-30 20:41:33 +0800

    upload the game

---
+++ GameSnake/assets/framework/scripts/PrefabManager.js
@@ -0,0 +1,39 @@
+
+cc.Class({
+    extends: cc.Component,
+    properties: {
+        snakeHead: cc.Prefab,
+        snakeBody: cc.Prefab,
+        wall: cc.Prefab,
+        apple: cc.Prefab,
+        stone: cc.Prefab,
+        water: cc.Prefab,
+        door: cc.Prefab,
+    },
+
+    /**
+     * 获取预制资源
+     * @param {*} prefabName 
+     */
+    getPrefabByName(prefabName) {
+        if (prefabName == "SnakeHead") {
+            return this.snakeHead;
+        } else if (prefabName == "SnakeBody") {
+            return this.snakeBody;
+        } else if (prefabName == "Wall") {
+            return this.wall;
+        } else if (prefabName == "Apple") {
+            return this.apple;
+        } else if (prefabName == "Stone") {
+            return this.stone;
+        } else if (prefabName == "Water") {
+            return this.water;
+        } else if (prefabName == "Door") {
+            return this.door;
+        }
+    },
+
+    init() {
+        return this;
+    },
+})
